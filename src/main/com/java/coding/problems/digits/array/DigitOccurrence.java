f09510fabe8de3ae757ffa79e59e24192a0215bb
Farzain Pathan <farzainpathan@gmail.com>
2022-06-20 17:56:57 +0530

    feat: added digit occurrence in array

---
+++ src/main/com/java/coding/problems/digits/array/DigitOccurrence.java
@@ -0,0 +1,62 @@
+package com.java.coding.problems.digits.array;
+
+import java.util.*;
+
+public class DigitOccurrence {
+  public List<Integer> countDigitOccurrence(List<Integer> inputList) {
+    List<Integer> digitList = new ArrayList<>();
+    Map<Integer, Integer> count = new HashMap<>();
+
+    inputList.forEach(
+        digit -> {
+          List<Integer> dList = convertToDigits(digit);
+          digitList.addAll(dList);
+        });
+
+    digitList.forEach(
+        digit -> {
+          if (count.containsKey(digit)) {
+            count.put(digit, count.get(digit) + 1);
+          } else {
+            count.put(digit, 1);
+          }
+        });
+
+    Map<Integer, Integer> resultMap = sortMap(count);
+
+    return findMostOccurrenceDigits(resultMap);
+  }
+
+  private List<Integer> findMostOccurrenceDigits(Map<Integer, Integer> resultMap) {
+
+    List<Integer> resultList = new ArrayList<>();
+    int maxValueInMap = (Collections.max(resultMap.values()));
+
+    for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
+      if (entry.getValue().equals(maxValueInMap)) {
+        resultList.add(entry.getKey());
+      }
+    }
+    return resultList;
+  }
+
+  private Map<Integer, Integer> sortMap(Map<Integer, Integer> unSortedMap) {
+
+    LinkedHashMap<Integer, Integer> reverseSortedMap = new LinkedHashMap<>();
+    unSortedMap.entrySet().stream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .forEachOrdered(value -> reverseSortedMap.put(value.getKey(), value.getValue()));
+    return reverseSortedMap;
+  }
+
+  private List<Integer> convertToDigits(Integer digit) {
+
+    List<Integer> result = new ArrayList<>();
+    String sNumber = String.valueOf(digit);
+    char[] charDigit = sNumber.toCharArray();
+    for (char c : charDigit) {
+      result.add(Character.getNumericValue(c));
+    }
+    return result;
+  }
+}
---
+++ src/test/com/java/coding/problems/digits/array/DigitOccurrenceTest.java
@@ -0,0 +1,38 @@
+package com.java.coding.problems.digits.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DigitOccurrenceTest {
+
+  @Test
+  @Order(1)
+  @DisplayName("Count the digit in given array")
+  public void test_1() {
+    // Given
+    DigitOccurrence digitOccurrence = new DigitOccurrence();
+    List<Integer> inputList = Arrays.asList(1, 12, 2, 32, 41, 56, 998);
+    // When
+    List<Integer> result = digitOccurrence.countDigitOccurrence(inputList);
+    // Then
+    assertThat(result).isEqualTo(Arrays.asList(1, 2));
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("Count the digit in given array")
+  public void test_2() {
+    // Given
+    DigitOccurrence digitOccurrence = new DigitOccurrence();
+    List<Integer> inputList = Arrays.asList(22, 23, 56, 34, 567, 90, 54, 3);
+    // When
+    List<Integer> result = digitOccurrence.countDigitOccurrence(inputList);
+    // Then
+    assertThat(result).isEqualTo(Arrays.asList(2, 3, 5));
+  }
+}
